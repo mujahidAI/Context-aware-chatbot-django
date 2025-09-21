72971ae46e1bfd7fb3d0d5439ac6c09aa86801a3
mujahidai <mr.mujahidislam1080@gmail.com>
2025-09-21 16:13:32 +0500

    initial commit

---
+++ chatbot/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+app_name = 'chatbot'
+
+urlpatterns = [
+    path('', views.chatbot, name='chatbot'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout'),
+    # path('chat/', views.chat, name='chat'),
+
+    path('password-reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', views.CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+]
